6215e076a2b3319b4813a458082c475d6d48377f
Jonathan Lin & Tom Mertz <student@epicodus.com>
2015-03-12 11:22:23 -0700

    Add code to pass oneLetterScoreFive test

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -9,6 +9,7 @@
                 'D' => 2, 'G' => 2,
                 'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3,
                 'F' => 4, 'H' => 4,
+                'K' => 5,
             ];
             $word_letters = str_split($word);
             $score_total = 0;
