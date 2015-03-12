43ba67005dcd345eb49e3c3cf6cd8d839537fa7f
Jonathan Lin & Tom Mertz <student@epicodus.com>
2015-03-12 11:16:42 -0700

    Add code to pass oneLetterScoreFour

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -7,7 +7,8 @@
             $scoreKeeper = [
                 'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1,
                 'D' => 2, 'G' => 2,
-                'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3
+                'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3,
+                'F' => 4
             ];
             $word_letters = str_split($word);
             $score_total = 0;
