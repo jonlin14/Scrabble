0e178a3ea6abe53a366d0d19b81c248dac51cac4
Jonathan Lin & Tom Mertz <student@epicodus.com>
2015-03-12 11:13:10 -0700

    Add code to pass 2 letters 6 score

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -5,9 +5,9 @@
         function getScore($word)
         {
             $scoreKeeper = [
-                'A' => 1, 'E' => 1, 'I' => 1,
+                'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1,
                 'D' => 2, 'G' => 2,
-                'B' => 3, 'C' => 3, 'M' => 3,
+                'B' => 3, 'C' => 3, 'M' => 3, 'P' => 3
             ];
             $word_letters = str_split($word);
             $score_total = 0;
