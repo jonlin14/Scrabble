40d0f53761d8f06b7e5ff5c319b8162b34dbb3dc
Jonathan Lin & Tom Mertz <student@epicodus.com>
2015-03-12 09:46:35 -0700

    Add specs for first half, up to 3 letter strings

---
+++ tests/ScrabbleTest.php
@@ -0,0 +1,56 @@
+<?php
+
+    class ScrabbleTest extends PHPUnit_Framework_TestCase
+    {
+        /*
+        input: "A" (string)
+        output: 1 (number)
+        Spec: Take a single letter "A" as a string and output the number 1
+        */
+
+        /*
+        input: "AE" (string)
+        output: 2 (number)
+        Spec: Take a two letter string "AE" and output the number 2
+        */
+
+        /*
+        input: "D" (string)
+        output: 2 (number)
+        Spec: Take a single letter "D" and output the number 2
+        */
+
+        /*
+        input: "AD" (str)
+        output: 3 (num)
+        Spec: Take a two letter string "AD" of different scores and give us the number 3
+        */
+
+        /*
+        input: "B" (string)
+        output: 3 (num)
+        Spec: Take a one letter string "B" and output the number 3
+        */
+
+        /*
+        input: "AB" (string)
+        output: 1 (number)
+        Spec: Take a two letter string of "AB" and return number 4
+        */
+
+        /*
+        input: "ADB" (string)
+        output: 6 (number)
+        Spec: Take a three letter string of "ADB" and return the number 6
+        */
+
+        /*
+        input:
+        output:
+        */
+    }
+
+
+
+
+ ?>
