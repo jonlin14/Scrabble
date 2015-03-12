f03329bd045780a7cc45e2767a250a75dd28661c
Jonathan Lin & Tom Mertz <student@epicodus.com>
2015-03-12 10:24:08 -0700

    Add code to pass second test

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -4,8 +4,13 @@
     {
         function getScore($word)
         {
-            $scoreKeeper = ['A' => 1];
-            return $scoreKeeper[$word];
+            $scoreKeeper = ['A', 'E' => 1];
+            $word_letters = str_split($word);
+            $score_total = 0;
+            foreach ($word_letters as $letter) {
+                $score_total += $scoreKeeper[$letter];
+            }
+            return $score_total;
         }
     }
 
