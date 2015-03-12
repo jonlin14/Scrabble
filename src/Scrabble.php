37aeb3f1477de9ae3a06e22af34d28a12946e8c7
Jonathan Lin & Tom Mertz <student@epicodus.com>
2015-03-12 10:29:56 -0700

    Add code to pass test 3

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -4,7 +4,7 @@
     {
         function getScore($word)
         {
-            $scoreKeeper = ['A' => 1, 'E' => 1];
+            $scoreKeeper = ['A' => 1, 'E' => 1, 'D' => 2];
             $word_letters = str_split($word);
             $score_total = 0;
             foreach ($word_letters as $letter) {
